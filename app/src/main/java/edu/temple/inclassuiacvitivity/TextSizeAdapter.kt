581cd13004d25c4c3aad112a8442c6d5bcb9f379
andie <andiethealto@gmail.com>
2023-02-09 12:20:01 -0500

    Changed getView to use getDropDownView to apply textSize

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -19,13 +19,28 @@ class TextSizeAdapter(_context: Context, _textSizes: Array<Int>) : BaseAdapter()
 
     override fun getItemId(p0: Int) = p0.toLong()
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val textView = TextView(context)
-        textView.textSize = textSizes[p0].toFloat()
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?) : View{
+        return (getDropDownView(p0,p1,p2) as TextView).apply {
+            textSize = 22f
+        }
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView: TextView
+
+        if(convertView == null){
+            textView = TextView(context)
+        }
+        else{
+            textView = convertView as TextView
+        }
+        textView.textSize = textSizes[position].toFloat()
         textView.setPadding(10,20,0,20)
-        textView.text = textSizes[p0].toString()
+        textView.text = textSizes[position].toString()
 
         return textView
+
+
     }
 
 }
